d85b3996d4bc3aebb644111be1dc92ab1e37c8fb
Ishan Prasad <ishanprasad.sahota@gmail.com>
2020-09-14 19:53:44 +0530

    initial commit

---
+++ server/routes/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+// const mongoose = require('mongoose');
+const router = express.Router();
+
+// const fs = require('fs');
+
+// const path = require('path');
+// const auth = require('http-auth');
+// const multer = require('multer');
+
+
+// var nodem = require('nodemailer');
+
+// const admindeets = auth.basic({
+//     file: path.join(__dirname, '../adminsdeets.htpasswd')
+// });
+
+module.exports = router;
---
+++ server/start.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+const app = require('./app');
+
+const server = app.listen(process.env.PORT, function(err)
+{
+    if(err) throw err;
+    console.log("Express runing on " + server.address().port);
+});
